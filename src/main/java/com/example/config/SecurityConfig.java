7d25334b16fdb3dab119db50a486c25c3a68f679
Hongjie ZHANG <hongjie.zhang@renault.com>
2019-08-02 10:27:09 +0200

    - update to spring boot 2.1.6  - restore all other deps  - find problem deps: tomcat

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.example.config;
 
-//@EnableWebFluxSecurity
-//@EnableReactiveMethodSecurity(order = Ordered.HIGHEST_PRECEDENCE)
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity(order = Ordered.HIGHEST_PRECEDENCE)
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
-//
-//        return http.authorizeExchange().pathMatchers("/**").permitAll().and().build();
-//
-//    }
+    @Bean
+    public SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
+
+        return http.authorizeExchange().pathMatchers("/**").permitAll().and().build();
+
+    }
 
 }
